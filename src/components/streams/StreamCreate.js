89d67676c490e37f9ac13645ebdd97ddc761f0db
John Constant <john@Johns-MacBook-Pro.local>
2020-11-24 22:09:51 +0000

    Create Stream form wit redux-form

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,7 +1,75 @@
-import React from "react";
+import React, { Component } from "react";
+import { Field, reduxForm } from "redux-form";
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
+class StreamCreate extends Component {
+  constructor(props) {
+    super(props);
+    this.renderInput = this.renderInput.bind(this);
+  }
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  }
+  renderInput({ label, input, meta }) {
+    const className = `field ${meta.error && meta.touched && "error"}`;
+    return (
+      <div className={className}>
+        <label>{label}</label>
+        {/* destructure the input object on formProps to get access to value and onChange properties */}
+        <input {...input} autoComplete="off" />
+        {/* {console.log(meta)} */}
+
+        {this.renderError(meta)}
+      </div>
+    );
+  }
+
+  onSubmit(formValues) {
+    console.log(formValues);
+  }
+  render() {
+    return (
+      <div>
+        <form
+          className="ui form error"
+          onSubmit={this.props.handleSubmit(this.onSubmit)}
+        >
+          <Field
+            name="title"
+            component={this.renderInput}
+            label="Enter name:"
+          />
+          <Field
+            name="description"
+            component={this.renderInput}
+            label="Enter description:"
+          />
+          <button className="ui basic button primary">Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+const validate = (formValues) => {
+  const errors = {};
+  if (!formValues.title) {
+    errors.title = "Please enter a name for your stream";
+  }
+
+  if (!formValues.description) {
+    errors.description = "Please enter a description for your stream";
+  }
+
+  return errors;
 };
 
-export default StreamCreate;
+export default reduxForm({
+  form: "streamCreate",
+  validate,
+})(StreamCreate);
