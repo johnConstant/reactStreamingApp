b18832202247abeb4db5c8d4d7474df34f2f04ce
John Constant <john@Johns-MacBook-Pro.local>
2020-11-26 21:56:49 +0000

    Add Streams action creators and reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,12 @@
-import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from "./types";
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  EDIT_STREAM,
+  DELETE_STREAM,
+} from "./types";
 import streams from "../apis/streams";
 
 const signIn = (userId) => {
@@ -19,4 +27,36 @@ const createStream = (formData) => async (dispatch) => {
   dispatch({ type: CREATE_STREAM, payload: response.data });
 };
 
-export { signIn, signOut, createStream };
+const fetchStreams = () => async (dispatch) => {
+  const response = await streams.get("/stream");
+
+  dispatch({ type: FETCH_STREAMS, payload: response.data });
+};
+
+const fetchStream = (id) => async (dispatch) => {
+  const response = await streams.get(`/streams/${id}`);
+
+  dispatch({ type: FETCH_STREAM, payload: response.data });
+};
+
+const editStream = (id, formData) => async (dispatch) => {
+  const response = await streams.put(`/streams/${id}`, formData);
+
+  dispatch({ type: EDIT_STREAM, payload: response.data });
+};
+
+const deleteStream = (id) => async (dispatch) => {
+  await streams.delete(`/streams/${id}`);
+
+  dispatch({ type: DELETE_STREAM, payload: id });
+};
+
+export {
+  signIn,
+  signOut,
+  createStream,
+  fetchStreams,
+  fetchStream,
+  editStream,
+  deleteStream,
+};
